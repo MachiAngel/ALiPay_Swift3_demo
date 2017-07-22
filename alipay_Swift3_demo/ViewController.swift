9f191136b7cdb19122bcd958d6b8903ae8bfe6d7
Shiang-Yu Huang <sky90456@hotmail.com>
2017-07-22 23:31:37 +0800

    Initial Commit

---
+++ alipay_Swift3_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  alipay_Swift3_demo
+//
+//  Created by Shiang-Yu Huang on 2017/7/22.
+//  Copyright © 2017年 Shiang-Yu Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
